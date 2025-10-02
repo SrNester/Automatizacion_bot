5229099abb5edc3e2f10dde615b543a2e20340e6
Kevin <kevin122_1@live.com>
2025-10-02 20:00:02 -0300

    carga

--- frontend/components/analytics.py
+++ frontend/components/analytics.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.graph_objects as go
 import plotly.express as px
+from plotly.subplots import make_subplots
+
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
